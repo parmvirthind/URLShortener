892c50e76320f2a6d28897f6a56729db854e1392
Parmvir <parmvirsthind@gmail.com>
2017-09-16 00:50:50 +0000

    Completing project

--- express_server.js
+++ express_server.js
@@ -1,125 +1,172 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
-var cookieParser = require('cookie-parser');
+const bcrypt = require("bcrypt");
+var cookieSession = require("cookie-session")
 
 
 app.set("view engine", "ejs");
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  secret: "lighthouse"
+}));
 
+
+
+app.use(function(request, response, next) {
+
+  response.locals = {
+
+    // user: {},
+    // password: request.session.password,
+    id: request.session.user_id,
+    email: request.session.email
+  };
+  next();
+});
+
+//Creates random ID
 function generateRandomString() {
   var randomID = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 6; i++)
+  for (var i = 0; i < 6; i++) {
     randomID += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
 
   return randomID;
 }
 
+//provides urls for respective user
+function urlsForUser(id) {
+  let uniqueDatabase = {};
+  for(i in urlDatabase) {
+    if(urlDatabase[i]["user_id"] === id) {
+      uniqueDatabase[i] = urlDatabase[i];
+    }
+  }
+  return uniqueDatabase;
+}
+
+//database of urls
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {url: "http://www.lighthouselabs.ca", user_id: 'andy'},
+  "9sm5xK": {url: "http://www.google.com", user_id: 'andy'},
 };
 
+//database of users
 const users = {
   "andy": {
     id: "andy",
     email: "andy@gmail.com",
-    password: "andyiscool"
+    hashedPassword: bcrypt.hashSync("andyiscool", 10)
   },
   "mijung": {
     id: "mijung",
     email: "mijung@gmail.com",
-    password: "mijungiscool"
+    hashedPassword: bcrypt.hashSync("mijungiscool", 10)
   }
 }
-
-app.use(function(request, response, next) {
-  response.locals = {
-
-    user: {},
-    password: request.cookies["password"],
-    id: request.cookies["user_id"]
-  };
-  next();
-});
 
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//homepage
 app.get("/", (request, response) => {
-  response.end("Hello!");
+  if(!response.locals.id) {
+    response.redirect(`/login`);
+  } else {
+    response.redirect(`/urls`);
+  }
 });
 
+//register page
 app.get("/register", (request, response) => {
   response.render('urls_registration');
 })
 
+//login page
 app.get("/login", (request, response) => {
   response.render('urls_login');
 })
 
+//page containing json
 app.get("/urls.json", (request, response) => {
   response.json(urlDatabase);
 });
 
-app.get("/hello", (request, response) => {
-  response.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+//page containing user's urls
 app.get("/urls", (request, response) => {
+
   let templateVars = {
-    urls: urlDatabase,
-    user: users[response.locals.id]
+    urls: urlsForUser(response.locals.id),
   };
   response.render('urls_index', templateVars);
 });
 
+//page to create new url
 app.get("/urls/new", (request, response) => {
-  response.render("urls_new");
+  if(!response.locals.id) {
+    response.redirect(`/login`);
+  } else {
+    response.render("urls_new");
+  }
 });
 
+//page for short url
 app.get("/urls/:id", (request, response) => {
-  let templateVars = {shortURL: request.params.id, longURL: urlDatabase[request.params.id]};
-  response.render("urls_show", templateVars);
+  let templateVars = {shortURL: request.params.id, longURL: urlDatabase[request.params.id]["url"], urlID: urlDatabase[request.params.id]["user_id"]};
+    response.render("urls_show", templateVars);
 });
 
+//page after updating url
 app.post("/urls", (request, response) => {
   let shortURL = generateRandomString();
   let longURL = request.body.longURL;
-  urlDatabase[shortURL] = longURL;
+  if(longURL[0] != "h") {
+    longURL = "http://" + longURL;
+  }
+
+  urlDatabase[shortURL] = { url: longURL, user_id: response.locals.id};
   response.redirect(`/urls/${shortURL}`);
 })
 
+//redirection to longurl (public)
 app.get("/u/:shortURL", (request, response) => {
-  let longURL = urlDatabase[request.params.shortURL];
+  var shortURL = request.params.shortURL;
+  let longURL = urlDatabase[shortURL]["url"];
   response.redirect(longURL);
 });
 
+//deletes short url
 app.post("/urls/:id/delete", (request, response) => {
   let shortURL = request.params.id;
-  delete urlDatabase[shortURL];
-  response.redirect(`/urls`);
-});
-
-app.post("/urls/:id/update", (request, response) => {
-  let updatedURL = request.body.longURL;
-  urlDatabase[request.params.id] = updatedURL
+  let matchID = urlDatabase[shortURL]["user_id"];
+  if(response.locals.id === matchID) {
+    delete urlDatabase[shortURL];
+  }
   response.redirect(`/urls`);
 })
 
+//goes to url page after updating link
+app.post("/urls/:id/update", (request, response) => {
+  let updatedURL = request.body.longURL;
+  urlDatabase[request.params.id] = {url: updatedURL, user_id: response.locals.id};
+  response.redirect(`/urls`);
+})
+
+//authorizes and authenticates user then redirects to urls
 app.post("/login", (request, response) => {
   let email = request.body.email;
   let password = request.body.password;
 
   for(i in users) {
     if(email === users[i].email) {
-      if(password === users[i].password) {
+      if(bcrypt.compareSync(password, users[i].hashedPassword)) {
         let id = users[i].id;
-        console.log(users);
-        response.cookie("user_id", id);
+        request.session.user_id = id;
+        request.session.email = email;
         response.redirect(`/urls`);
         return;
       }
@@ -129,17 +176,19 @@ app.post("/login", (request, response) => {
   return;
 })
 
+//logs out and clears cookies
 app.post("/logout", (request, response) => {
-  response.clearCookie("user_id");
+  request.session = null;
   response.redirect(`/login`);
 })
 
+//checks valid register and redirects to urls
 app.post("/register", (request, response) => {
   let id = generateRandomString();
   let email = request.body.email;
-  let password = request.body.password;
+  const hashedPassword = bcrypt.hashSync(request.body.password, 10);
 
-  if(email === "" || password === "") {
+  if(email === "" || hashedPassword === "") {
     response.status(400).send("Bad Request");
     return;
   }
@@ -149,8 +198,9 @@ app.post("/register", (request, response) => {
       return;
     }
   }
-  users[id] = {id, email, password};
-  response.cookie("user_id", id);
+  users[id] = {id, email, hashedPassword};
+  request.session.user_id = id;
+  request.session.email = email;
   response.redirect(`/urls`);
 })
 
